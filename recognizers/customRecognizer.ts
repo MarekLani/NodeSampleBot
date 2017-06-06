a7b560e60ff0ec7f391a7192e9276f72d6349cb1
Marek <mareklani@gmail.com>
2017-06-06 18:02:26 +0200

    Added custom recognizer

---
+++ recognizers/customRecognizer.ts
@@ -0,0 +1,17 @@
+module.exports = {
+recognize: function (context, done) {
+  var intent = { score: 0.0, intent: '' };
+
+        if (context.message.text) {
+            switch (context.message.text.toLowerCase()) {
+                case 'help':
+                    intent = { score: 1.0, intent: 'Help' };
+                    break;
+                case 'goodbye':
+                    intent = { score: 1.0, intent: 'Goodbye' };
+                    break;
+            }
+        }
+        done(null, intent);
+    }
+}
